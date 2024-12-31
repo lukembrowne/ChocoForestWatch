851b5e8e0581082978506b77c45fb56850fd081e
Luke Browne <lukembrowne@gmail.com>
2024-12-31 15:13:57 -0500

    update version

--- backend/djangocfw/djangocfw/version.py
+++ backend/djangocfw/djangocfw/version.py
@@ -1,6 +1,6 @@
 # Format: (major, minor, patch, release_stage)
 # Release stages: 'alpha', 'beta', 'rc', 'final'
-VERSION = (0, 1, 0, 'beta')
+VERSION = (0, 1, 1, 'beta')
 
 def get_version():
     version = '%s.%s.%s' % (VERSION[0], VERSION[1], VERSION[2])
