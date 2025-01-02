a2fa2c2354af229224e157eecf8aa7bfc2e42040
Luke Browne <lukembrowne@gmail.com>
2025-01-02 09:56:06 -0500

    updating api urls

--- frontend/src/services/auth.js
+++ frontend/src/services/auth.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL || 'https://chocoforestwatch.fcat-ecuador.org/api/';
 
 class AuthService {
     async login(username, password, remember = false) {
-        const response = await axios.post(API_URL + 'auth/login/', {
+        const response = await axios.post(`${API_URL}auth/login/`, {
             username,
             password
         });
