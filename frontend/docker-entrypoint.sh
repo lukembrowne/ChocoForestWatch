abafd2c75dc79a3922c2059836e5e647c6fa1d2b
Luke Browne <lukembrowne@gmail.com>
2025-01-03 11:27:22 -0500

    updating build

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,9 +1,2 @@
 #!/bin/sh
-
-if [ "$DJANGO_DEBUG" = "True" ]; then
-    echo "Starting Quasar in development mode..."
-    npx quasar dev --hostname 0.0.0.0
-else
-    echo "Starting nginx in production mode..."
-    nginx -g 'daemon off;'
-fi 
+exec "$@" 
