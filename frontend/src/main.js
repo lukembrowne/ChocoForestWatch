34cd91f7933d407e460ed876a7daa039a8df5b7a
Luke Browne <lukembrowne@gmail.com>
2025-05-14 13:50:25 -0400

    remove sentry logging

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -12,23 +12,23 @@ import 'quasar/dist/quasar.css'
 const app = createApp(App)
 
 // Initialize Sentry
-Sentry.init({
-  app,
-  dsn: import.meta.env.VITE_SENTRY_DSN,
-  integrations: [
-    Sentry.browserTracingIntegration({ router }),
-    Sentry.replayIntegration(),
-  ],
+// Sentry.init({
+//   app,
+//   dsn: import.meta.env.VITE_SENTRY_DSN,
+//   integrations: [
+//     Sentry.browserTracingIntegration({ router }),
+//     Sentry.replayIntegration(),
+//   ],
   
-  // Performance Monitoring
-  tracesSampleRate: 1.0,
-  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+//   // Performance Monitoring
+//   tracesSampleRate: 1.0,
+//   tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
   
-  // Session Replay
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
-  environment: import.meta.env.MODE
-})
+//   // Session Replay
+//   replaysSessionSampleRate: 0.1,
+//   replaysOnErrorSampleRate: 1.0,
+//   environment: import.meta.env.MODE
+// })
 
 app.use(Quasar, {
   plugins: { Dialog }
